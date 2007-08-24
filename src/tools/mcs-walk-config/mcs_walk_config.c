f54d34517e42a0d9f9abd44c5c072d647f95e1f3
William Pitcock <nenolod@atheme-project.org>
2007-08-23 19:18:42 -0500

    Add mcs-walk-config, displays a configuration structure in the mcs configuration database.

---
+++ src/tools/mcs-walk-config/mcs_walk_config.c
@@ -0,0 +1,74 @@
+/*
+ * This is mcs; a modular configuration system.
+ *
+ * Copyright (c) 2007 William Pitcock <nenolod -at- sacredspiral.co.uk>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "libmcs/mcs.h"
+
+int
+main(int argc, char *argv[])
+{
+	mcs_handle_t *h;
+	char *foo = NULL;
+	mcs_list_t *groups, *i;
+
+	if (argc < 2)
+	{
+		printf("usage: %s domain\n", argv[0]);
+		return -1;
+	}
+
+	mcs_init();
+
+	h = mcs_new(argv[1]);
+	groups = mcs_get_sections(h);
+
+	for (i = groups; i != NULL; i = i->next)
+	{
+		mcs_list_t *keys, *i2;
+
+		printf("%s\n", i->data);
+		keys = mcs_get_keys(h, i->data);
+
+		for (i2 = keys; i2 != NULL; i2 = i2->next)
+		{
+			printf("  %c-- %s", i2->next != NULL ? '|' : '`');
+			free(i2->data);
+		}
+
+		mcs_list_free(keys);
+		free(i->data);
+	}
+
+	mcs_list_free(groups);
+	mcs_fini();
+
+	return 0;
+}
